b1ca2b45d80b02e3833103325ff07425ee81a9ff
Spencer Depas <spencerdepas@gmail.com>
2022-06-29 00:08:11 -0400

    Work on school home screen list UI

--- app/src/main/java/spencerdepas/nycschools/adapter/BindingAdapters.java
+++ app/src/main/java/spencerdepas/nycschools/adapter/BindingAdapters.java
@@ -1,13 +1,17 @@
 package spencerdepas.nycschools.adapter;
 
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.time.Month;
 import java.util.List;
 
+import spencerdepas.nycschools.R;
 import spencerdepas.nycschools.model.School;
 
 public class BindingAdapters {
@@ -32,6 +36,15 @@ public class BindingAdapters {
     public static void bindLayoutManager(@NonNull RecyclerView recyclerView, boolean vertical) {
         int orientation = vertical ? RecyclerView.VERTICAL : RecyclerView.HORIZONTAL;
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext(), orientation, false));
+    }
+
+    @BindingAdapter("attendanceRate")
+    public static void bindChefTabAdapter(TextView textView,
+                                          String attendanceRate) {
+
+
+        String attendenceRate = textView.getContext().getString(R.string.attendence_rate) + " " + attendanceRate.substring(0, 3);
+        textView.setText(attendenceRate);
     }
 
 }
